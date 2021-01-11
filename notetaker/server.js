986fb097d0b633760767363a21036efc0ccf0b6f
bennettcollier <71052027+bennettcollier@users.noreply.github.com>
2021-01-10 22:54:46 -0800

    Add files via upload

---
+++ notetaker/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}`)
+});
